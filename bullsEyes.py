2c30512aef4322a0368c38a5362cfb9528ffdb21
csPython <cspython@AD25TF04NGF1J-85.local>
2019-03-11 10:56:47 -0400

    added custom function for bullseyes

--- bullsEyes.py
+++ bullsEyes.py
@@ -1,7 +1,13 @@
 from graphics import*
 
-def circle(pos, rad, color):
-    c = Circle(Point(pos, pos), rad-(20*(i+1)))
+
+def bulleye(ra,ran, pos, rad, color):
+    for ra in range(ran):
+        circle(pos, ra, rad, color)
+        color = swap[color]
+
+def circle(pos, ran, rad, color):
+    c = Circle(Point(pos, pos), rad-(20*(ran+1)))
     c.setFill(color_rgb(color,0,0))
     c.draw(wind)
     
@@ -13,6 +19,5 @@ swap = {0: 244, 244:0}
 #create window
 wind = GraphWin("checker", winX, winX)
 wind.setCoords(0, 0, 800, 800)
-for i in range(9):
-    circle(winX/2, 200, red)
-    red = swap[red]
+i = 0
+bulleye(i,9, winX/2, 200, red)
