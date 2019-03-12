045e4024fa2bca9b252ae983aa659ef3584b2089
csPython <cspython@AD25TF04NGF1J-85.local>
2019-03-12 11:56:32 -0400

    change color variable and user can input how many bullseyes to spawn. Also change position to random

--- bullsEyes.py
+++ bullsEyes.py
@@ -1,23 +1,26 @@
 from graphics import*
+import random
 
-
-def bulleye(ra,ran, pos, rad, color):
+def bulleye(ra,ran, pos,pos2, rad, color):#bullseyes function
     for ra in range(ran):
-        circle(pos, ra, rad, color)
-        color = swap[color]
+        circle(pos,pos2, ra, rad, color)
+        color += 25
 
-def circle(pos, ran, rad, color):
-    c = Circle(Point(pos, pos), rad-(20*(ran+1)))
-    c.setFill(color_rgb(color,0,0))
+def circle(pos, pos2, ran, rad, color):#circle function
+    c = Circle(Point(pos, pos2), rad-(20*(ran+1)))
+    c.setFill(color_rgb(color,0,color))
     c.draw(wind)
     
 #window size
 winX = 800
 #color for red and black
-red = 244
+col = 0
 swap = {0: 244, 244:0}
 #create window
 wind = GraphWin("checker", winX, winX)
 wind.setCoords(0, 0, 800, 800)
 i = 0
-bulleye(i,9, winX/2, 200, red)
+print("how many bullseyes")
+hm = int(input())
+for j in range(hm):
+    bulleye(i,9, random.randint(0,800),random.randint(0,800), 200, col)
