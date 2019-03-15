0b8fa98347751ddec23f2fbf2e0a69fea0ccf7b3
csPython <cspython@AD25TF04NGF1J-85.local>
2019-03-15 09:34:23 -0400

    fixed where some bullsEyes don't look like bullseye

--- bullsEyes.py
+++ bullsEyes.py
@@ -1,16 +1,17 @@
 from graphics import*
 import random
 
-def bulleye(ra,ran, pos,pos2,spac, rad, red,green,blue):#bullseyes function
+def bulleye(ra,ran, pos,pos2, rad, red,green,blue):#bullseyes function
     for ra in range(ran):
         randnum = random.randint(1,3)
-        circle(pos,pos2,spac, ra, rad, red,green,blue)
+        circle(pos,pos2,rad/10, ra, rad, red,green,blue)
+        #random rgb 
         if randnum == 1:
-            red += 20
+            red += 25
         elif randnum == 2:
-            green += 20
+            green += 25
         elif randnum == 3:
-            blue += 20
+            blue += 25
         
 
 def circle(pos, pos2,spac, ran, rad, red,green,blue):#circle function
@@ -20,12 +21,11 @@ def circle(pos, pos2,spac, ran, rad, red,green,blue):#circle function
 check = False    
 #window size
 winX = 800
-#color for red and black
-
-swap = {0: 244, 244:0}
+radi = random.randint(0,800)
 #create window
 wind = GraphWin("checker", winX, winX)
 wind.setCoords(0, 0, 800, 800)
+#user input/protection from bad input
 while check == False:
     try:
         hm = int(input("how many bullseyes"))
@@ -36,6 +36,6 @@ while check == False:
     except ValueError:
         print("please input a number")
     
-    
+#spawn bullseyes    
 for j in range(hm):
-    bulleye(0,10, random.randint(0,800),random.randint(0,800),random.randint(10,30), random.randint(150,300), 0,0,0)
+    bulleye(0,10, random.randint(0,800),radi, random.randint(150,300), 0,0,0)
